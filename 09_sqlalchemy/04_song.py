e14f14d646cb28c18403c6d5786a175cda4dcb48
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-11-11 10:21:42 +0100

    modelo base de datos sqlalchemy

--- 09_sqlalchemy/04_song.py
+++ 09_sqlalchemy/04_song.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Integer, String, Boolean
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
 
 # configurar base de datos
 
@@ -18,3 +18,24 @@ SessionLocal = sessionmaker(
     autoflush=True,
     expire_on_commit=False
 )
+
+# modelo base de datos (sqlalchemy)
+
+# clase base para modelos sqlalchemy
+class Base(DeclarativeBase):
+    pass
+
+# modelo de la tabla song (se crea sólo un modelo, que será una tabla en nuestra base de datos)
+class Song(Base):
+    __tablename__ = "songs" # nombre de la tabla en bd
+    
+    # clave primaria, se genera automáticamente
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    # requerido, máximo 200 caracteres
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    # requerido, máximo 200 caracteres
+    artist: Mapped[str] = mapped_column(String(200), nullable=False)
+    # opcional
+    duration_seconds: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    # optional
+    explicit: Mapped[bool | None] = mapped_column(Boolean, nullable=True)
