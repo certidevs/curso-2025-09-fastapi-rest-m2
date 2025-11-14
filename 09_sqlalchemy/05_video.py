65032878b784317bca41bfe71810bc059acbdcd8
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-11-14 10:06:09 +0100

    estructura para entidad video

---
+++ 09_sqlalchemy/05_video.py
@@ -0,0 +1,34 @@
+"""
+VIDEO
+- id: int
+- title: string (obligatorio)
+- channel: string (obligatorio)
+- views: entero (opcional)
+- has_subtitles: booleano (opcional)
+"""
+
+# CONFIGURACIÓN DE BASE DE DATOS
+
+
+# MODELO DE BASE DE DATOS (SQLALCHEMY)
+
+
+# MODELOS PYDANTIC (SCHEMAS)
+
+
+# INICIALIZACIÓN DE BASE DE DATOS
+
+
+# DEPENDENCIA DE FASTPI
+
+
+# APLICACIÓN FASTAPI
+
+
+# ENDPOINTS CRUD (Create, Read, Update, Delete)
+"""
+Create: Método POST (create)
+Read: Método GET (find_all y find_by_id)
+Update: Método PUT (update_full) y método PATCH (update_partial)
+Delete: Método DELETE (delete)
+"""
